e45735233f4c926b2987bc3bff279e35ad98c50d
rayque alencar <rayquealencar45452@gmail.com>
2023-10-27 13:47:14 -0300

    add imprimirSolucao

--- Solucao.hpp
+++ Solucao.hpp
@@ -7,19 +7,13 @@
 #include <fstream>
 
 using namespace std;
-class Solucao
-{
+class Solucao{
     public:
         int custoTotal, custoTerceirizacao, custoVeiculo, custoRotas;
         vector<vector<int>> rotas;
         vector<int> terceirizados;
 
         Solucao(vector<vector<int>> rotas, vector<int> terceirizados, int custoTotal, int custoTerceirizacao, int custoVeiculo, int custoRotas);
-    
-    private:
-    /* data */
 };
-
-
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "Instancia.hpp"
 #include "Solucao.hpp"
 
-
+//funçãao que calcula o custo das rotas
 int custoRotas(vector<vector<int>> rotas, vector<vector<int>> custo){
     int custoRotas = 0;
     for(vector<int> rota : rotas){
@@ -13,62 +13,68 @@ int custoRotas(vector<vector<int>> rotas, vector<vector<int>> custo){
     return custoRotas;
 }
 
+//função que calcula o custo da terceirização
 int custoTerceirizacao(vector<int> terceirizados, vector<int> custosTerceirizacao){
+    
     int custoTerceirizacao = 0;
-    for (int i = 0; i < int(terceirizados.size()); i++)
-    {
+    for (int i = 0; i < int(terceirizados.size()); i++){
         custoTerceirizacao += custosTerceirizacao[terceirizados[i]-1];
     }
     return custoTerceirizacao;
 }
 
+//função que calcula o custo de ultilização dos veiculos
 int custoVeiculos(int custoVeiculo, vector<vector<int>> rotas){
+   
     int custoVeiculos = 0;
     for(vector<int> rota : rotas){
-        if (rota[0] != rota[1])
-        {
+        if (rota[0] != rota[1]){
             custoVeiculos += custoVeiculo;
         }
     }
     return custoVeiculos;
 }
 
+//função que implementa o algoritmo guloso
 Solucao guloso(int nVeiculos, int nEntregas, int capacidadeVeiculo, int minEntregas,int custoVeiculo, vector<int> demandas, vector<vector<int>> custo, vector<int> custosTerceirizacao){
+
+    //inicialização das variaveis 
     vector<vector<int>> rotas(nVeiculos);
     vector<int> terceirizacao;
     vector<int> pesos(nVeiculos, 0);
     vector<int> candidatos(nEntregas);
 
-    for (int i = 1; i <= nEntregas; i++)
-    {
+    //inicialização do vetor de candidatos
+    for (int i = 1; i <= nEntregas; i++){
         candidatos[i-1] = i;
     }
 
-    for (int v = 0; v < nVeiculos; v++)
-    {
+    //inicialização das rotas
+    for (int v = 0; v < nVeiculos; v++){
         rotas[v].push_back(0);
         pesos.push_back(0);
     }
-    
-    while (!candidatos.empty())
-    {
+
+    //algoritmo guloso  
+    while (!candidatos.empty()){
         int iMelhorCandidato = 0;
         int melhorV = 0;
         int melhorCusto = 999999999;
+
+        //encontra o melhor candidato, quer é o que tem o menor custo de arco
         for(int i = 0 ; i < int(candidatos.size()); i++){
-            for (int v = 0; v < nVeiculos; v++)
-            {
+            for (int v = 0; v < nVeiculos; v++){
                 int custoArco;
 
-                if (rotas[v].back() == 0)
-                {
+        
+                if (rotas[v].back() == 0){
                     custoArco = custo[rotas[v].back()][candidatos[i]]+custoVeiculo;
                 }else{
                     custoArco = custo[rotas[v].back()][candidatos[i]];
                 }
 
-                if (custoArco < melhorCusto && pesos[v] + demandas[candidatos[i]-1] <= capacidadeVeiculo)
-                {
+                //verifica se o candidato é melhor que o melhor candidato atual e se o veiculo tem capacidade para atender o candidato
+                if (custoArco < melhorCusto && pesos[v] + demandas[candidatos[i]-1] <= capacidadeVeiculo){
                     iMelhorCandidato = i;
                     melhorCusto = custoArco;
                     melhorV = v;
@@ -78,111 +84,94 @@ Solucao guloso(int nVeiculos, int nEntregas, int capacidadeVeiculo, int minEntre
 
         int melhorCandidato = candidatos[iMelhorCandidato];
 
-        if (nEntregas-int(candidatos.size()) > minEntregas && custosTerceirizacao[melhorCandidato] < melhorCusto)
-        {
+        if (nEntregas-int(candidatos.size()) > minEntregas && custosTerceirizacao[melhorCandidato] < melhorCusto){
             terceirizacao.push_back(melhorCandidato);
         }
-        else
-        {
+        else{
             rotas[melhorV].push_back(melhorCandidato);
             pesos[melhorV] += demandas[melhorCandidato-1];
         }
+        
         //cout << "--------------------------" << endl;
         //cout << "Melhor candidato: " << melhorCandidato << endl;
         //cout << "Melhor custo: " << melhorCusto << endl;
         //cout << "Melhor veiculo: " << melhorV << endl;
         //cout << "Peso: " << pesos[melhorV] << endl;
         //cout << "Retirei: " << candidatos[iMelhorCandidato] << endl;
+
         candidatos.erase(candidatos.begin()+iMelhorCandidato);
 
-        //for (int i = 0; i < nVeiculos; i++)
-        //{
-        //    cout << "Rota " << i << ": ";
-        //    for (int j = 0; j < int(rotas[i].size()); j++)
-        //    {
-        //        cout << rotas[i][j] << " ";
-        //    }
-        //    cout << endl;
-        //}
-
-        //cout << "Terceirizacao: ";
-        //for (int i = 0; i < int(terceirizacao.size()); i++)
-        //{
-        //    cout << terceirizacao[i] << " ";
-        //}
-        //cout << endl;      
+   
     }
-    for (int v = 0; v < nVeiculos; v++)
-    {
+
+    //adiciona o deposito no final de cada rota
+    for (int v = 0; v < nVeiculos; v++){
         rotas[v].push_back(0);
     }
 
+    //calcula o custo da solução
     int custoRotasSolucao = custoRotas(rotas, custo);
     int custoTerceirizacaoSolucao = custoTerceirizacao(terceirizacao, custosTerceirizacao);
     int custoVeiculosSolucao = custoVeiculos(custoVeiculo, rotas);
     int custoTotal = custoRotasSolucao + custoTerceirizacaoSolucao + custoVeiculosSolucao;
 
+    //cria a solução
     Solucao solucaoGulosa(rotas, terceirizacao, custoTotal, custoTerceirizacaoSolucao, custoVeiculosSolucao, custoRotasSolucao);
-    cout << "--------------------------" << endl;
-    for (int i = 0; i < nVeiculos; i++)
-    {
-        cout << "Rota " << i << ": ";
-        for (int j = 0; j < int(rotas[i].size()); j++)
-        {
-            cout << rotas[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << "Custo das rotas: " << custoRotas(rotas, custo) << endl;
-    cout << "Terceirizacao: ";
-    for (int i = 0; i < int(terceirizacao.size()); i++)
-    {
-        cout << terceirizacao[i] << " ";
-    }
-    cout << endl;
-    cout << "Custo da terceirizacao: " << custoTerceirizacao(terceirizacao, custosTerceirizacao) << endl;
-    cout << "--------------------------" << endl;
     return solucaoGulosa;
 }
 
+//função que imprime a solução do algoritmo guloso
+void imprimirSolucao(Solucao solucao){
+
+    cout << "Custo Total: " << solucao.custoTotal << endl;
+    cout << "Custo das rotas: " << solucao.custoRotas << endl;
+    cout << "Custo de ultilizacao dos veiculos: " << solucao.custoVeiculo << endl;
+    cout << "Custo da terceirizacao: " << solucao.custoTerceirizacao << endl;
+
+    cout << endl;
+
+    cout << "Clientes Terceirizados: ";
+    for (int i = 0; i < int(solucao.terceirizados.size()); i++){
+        cout << solucao.terceirizados[i] << " ";
+    }
+
+    cout << endl;
+
+    int nRotas = 0;
+    for(vector<int> rota : solucao.rotas){
+        if (rota[0] != rota[1]){
+            nRotas += 1;
+        }
+    }
+
+    cout << "\nNumero de rotas: " << nRotas << endl;
+
+    cout << endl;
+
+    for (int i = 0; i < int(solucao.rotas.size()); i++){
+        
+        if(solucao.rotas[i][0] != solucao.rotas[i][1]){
+
+            cout << "Rota " << i + 1 << ": ";
+
+            for (int j = 0; j < int(solucao.rotas[i].size()); j++){
+                cout << solucao.rotas[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+    cout << endl;
+}
 
 int main() {
     
-    string nomeArquivo = "instancias/arquivo_1.txt";
+    string nomeArquivo = "instancias/n199k17_D.txt";
     Instancia instancia(nomeArquivo);
 
     instancia.imprimirDados();
 
-    //instancia.guloso();
     Solucao solucaoGulosa = guloso(instancia.nVeiculos, instancia.nEntregas, instancia.capacidadeVeiculo, instancia.minEntregas, instancia.custoVeiculo, instancia.demandas, instancia.custo, instancia.custosTerceirizacao);
-    //guloso(instancia.nVeiculos, instancia.nEntregas, instancia.capacidadeVeiculo, instancia.minEntregas, instancia.custoVeiculo, instancia.demandas, instancia.custo, instancia.custosTerceirizacao);
-    cout << "Custo Total: " << solucaoGulosa.custoTotal << endl;
-    cout << "Custo das rotas: " << solucaoGulosa.custoRotas << endl;
-    cout << "Custo da terceirizacao: " << solucaoGulosa.custoTerceirizacao << endl;
-    cout << "Custo dos veiculos: " << solucaoGulosa.custoVeiculo << endl;
-
-    cout << "--------------------------" << endl;
-
-    for (int i = 0; i < instancia.nVeiculos; i++)
-    {
-        cout << "Rota " << i << ": ";
-        for (int j = 0; j < int(solucaoGulosa.rotas[i].size()); j++)
-        {
-            cout << solucaoGulosa.rotas[i][j] << " ";
-        }
-        cout << endl;
-    }
-
-    cout << "--------------------------" << endl;
-
-    cout << "Terceirizacao: ";
-    for (int i = 0; i < int(solucaoGulosa.terceirizados.size()); i++)
-    {
-        cout << solucaoGulosa.terceirizados[i] << " ";
-    }
-    cout << endl;
-
-    cout << "--------------------------" << endl;
+    imprimirSolucao(solucaoGulosa);
 
     return 0;
 }
